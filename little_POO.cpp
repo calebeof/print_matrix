6f1799d78d9d7b21413915576d19705b0790690f
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2018-12-09 11:34:57 -0200

    Update little_POO.cpp

--- little_POO.cpp
+++ little_POO.cpp
@@ -3,54 +3,73 @@
 using namespace std;
 
 class Car {
-  int currentSpeed = 0;
-  public: 
-    string model;
-    float power;
-    int maxSpeed;
-  
-  void printIt(){
-    cout << "Model: " << model << "\n";
-    cout << "Power: " << power << "\n";
-    cout << "Max Speed: " << maxSpeed << "\n";
-    cout << "Current Speed: " << currentSpeed << "\n";
-  }
-  
-  void speedUp(){
-    cout << "Acelerando... Velocidade atual: " << currentSpeed++ << "\n";
-  }
-  void brake() {
-    cout << "Freando... Velocidade atual: " << currentSpeed-- << "\n";
-  }
+    private:
+      int currentSpeed;
+      string model;
+      float power;
+      int maxSpeed;
+    public:
+      Car(string m, float p, int max){
+        setModel(m);
+        setPower(p);
+        setMaxSpeed(max);
+        setCurrentSpeed(0);
+      }
+      void setModel(string m){
+        this->model = m;
+      }
+      void setPower(float p){
+        this->power = p;
+      }
+      void setMaxSpeed(int max){
+        this->maxSpeed = max;
+      }
+      void setCurrentSpeed(int s){
+        this->currentSpeed = s;
+      }
+      string getModel(){
+        return this->model;
+      }
+      float getPower(){
+        return this->power;
+      }
+      int getMaxSpeed(){
+        return this->maxSpeed;
+      }
+      int getCurrentSpeed(){
+        return this->currentSpeed;
+      }
+      void printIt(){
+        cout << "Model: " << getModel() << "\n";
+        cout << "Power: " << getPower() << "\n";
+        cout << "Max Speed: " << getMaxSpeed() << "\n";
+        cout << "Current Speed: " << getCurrentSpeed() << "\n";
+      }
+      
+      void speedUp(){
+        setCurrentSpeed(getCurrentSpeed()+1);
+        cout << "Acelerando... Velocidade atual do " << getModel()<< ": " << getCurrentSpeed() << "\n";
+      }
+      void brake() {
+        setCurrentSpeed(getCurrentSpeed()-1);
+        cout << "Freando... Velocidade atual do " << getModel()<< ": " << getCurrentSpeed() << "\n";
+      }
 };
 
 int main() {
-  Car camaro;
-  Car onix;
-  Car ferrari;
-  
-  camaro.model = "Camaro";
-  camaro.power = 4.2;
-  camaro.maxSpeed = 350;
-  onix.model = "Onix";
-  onix.power = 6.2;
-  onix.maxSpeed = 150;
-  ferrari.model = "Ferrari";
-  ferrari.power = 6.2; 
-  ferrari.maxSpeed = 350;
-  
+  Car camaro("Camaro", 4.2, 350);
+  Car onix("Onix", 6.2, 150);
+  Car ferrari("Ferrari", 6.2, 350);
   vector <Car> cars;
   cars.push_back (camaro);
   cars.push_back(ferrari);
   cars.push_back(onix);
   
-  for (int i=0; i<10; i++){
+  for (int i=0; i<10; i++)
     camaro.speedUp();
-  }
-  for (int i=0; i<10; i++){
+  for (int i=0; i<10; i++)
     camaro.brake();
-  }
-  for (int i=0; i<3; i++){
+  for (int i=0; i<cars.size(); i++){
     cout << "\n";
     cars[i].printIt();
   }
