c7ab1fe393d3af56f6c01888f7334f81867f094b
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2019-01-27 22:16:47 -0200

    Macros iniciais

---
+++ Macros.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define ll long long int 
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define vll vector<ll>
+#define pqi priority_queue<int>
+#define pii pair<int, int> 
+#define vii vector<pii>
+#define vvii vector<vii>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define loop(n) for(int i=0; i<n; i++)
+#define LOOP(k,n) for(int i=k; i<n; i++)
+#define POOL(k,n) for(int i=k; i>=n; i--)
+#define endl '\n'
+
+using namespace std;
+
+int main () {
+  ios_base::sync_with_stdio(false);
+  return 0;
+}
