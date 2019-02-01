658d9150d5259dd25c06ce3f87609f7c57b67057
Calebe Oliveira <30943380+calebeof@users.noreply.github.com>
2019-02-01 15:23:59 -0300

    Adicionei watch ao template

--- Macros.cpp
+++ Macros.cpp
@@ -1,10 +1,19 @@
+#define TEMPLATE_VERSION "v0.1 01/02/2019"
 #include <bits/stdc++.h>
 
-#define ll long long int 
+#define MOD 1000000007LL
+#define INF INT_MAX
+#define LINF LL_MAX
+#define LL long long int 
+#define ULL unsigned long long int
+#define LD long double
 #define vi vector<int>
 #define vb vector<bool>
 #define vs vector<string>
-#define vll vector<ll>
+#define si set<int>
+#define qi queue<int>
+#define sti stack<int>
+#define vLL vector<LL>
 #define pqi priority_queue<int>
 #define pii pair<int, int> 
 #define vii vector<pii>
@@ -13,20 +22,21 @@
 #define mp make_pair
 #define f first
 #define s second
-#define loop(n) for(int i=0; i<n; i++)
+#define EPS 1e-9
+#define loop( n ) for(int i=0; i<n; i++)
 #define LOOP(k,n) for(int i=k; i<n; i++)
 #define POOL(k,n) for(int i=k; i>=n; i--)
 #define FIND(v, value) find(v.begin(), v.end(), value)
-#define SORT(v) sort(v.begin(), v.end())
+#define SORT( v ) sort(v.begin(), v.end())
 #define SORTC(v, comp) sort(v.begin(), v.end(), comp)
-#define EOF while (true)
-#define INF INT_MAX
-#define TAM 1000000
+#define WW( x ) cout<<#x<<" = "<<(x)<<"\n";
+#define TAM (1000000)
 #define endl '\n'
 
 using namespace std;
 
 int main () {
   ios_base::sync_with_stdio(false);
+  cout << setprecision(2) << fixed;
   return 0;
 }
