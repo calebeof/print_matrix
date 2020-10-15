28d0e5e7d14d6b3dab538e4ad07505ed87856eb6
calebeof <calebeof14@gmail.com>
2020-10-15 15:39:50 -0300

    Editando macros com debug

--- Macros.cpp
+++ Macros.cpp
@@ -1,4 +1,4 @@
-#define TEMPLATE_VERSION "v0.2 06/03/2019"
+#define TEMPLATE_VERSION "v0.2 15/10/2020"
 #include <bits/stdc++.h>
 
 #define MOD (1000000007LL)
@@ -27,18 +27,79 @@
 #define EPS 1e-9
 #define loop( n ) for(int i=0; i<n; i++)
 #define LOOP(k,n) for(int i=k; i<n; i++)
-#define POOL(k,n) for(int i=k; i>=n; i--)
 #define FIND(v, value) find(v.begin(), v.end(), value)
 #define SORT( v ) sort(v.begin(), v.end())
 #define SORTC(v, comp) sort(v.begin(), v.end(), comp)
-#define WW( x ) cout<<#x<<" = "<<(x)<<"\n";
 #define endl '\n'
 
 using namespace std;
 
+#ifdef DEBUG
+    template <template <class, class> class Container, class T, class Alloc = std::allocator<T> >
+        std::ostream &operator<<(
+            std::ostream &os,
+            const Container<T, Alloc> &container)
+        {
+            const char *sep[] = {"", ", "};
+            int s = 0;
+            os << "[ ";
+            for (const T &elt : container)
+            {
+                os << sep[s] << elt;
+                s = 1;
+            }
+            return os << " ]";
+        }
+
+        template <typename T1, typename T2>
+        std::ostream &operator<<(std::ostream &os,
+                                const std::map<T1, T2> &m)
+        {
+            const char *sep[] = {"", ", "};
+            int s = 0;
+            os << "{ ";
+            for (const auto &elt : m)
+            {
+                os << sep[s] << elt.first << ": " << elt.second;
+                s = 1;
+            }
+            return os << " }";
+        }
+
+        template <typename T1>
+        std::ostream &operator<<(std::ostream &os,
+                                const std::set<T1> &m)
+        {
+            const char *sep[] = {"", ", "};
+            int s = 0;
+            os << "{ ";
+            for (const auto &elt : m)
+            {
+                os << sep[s] << elt;
+                s = 1;
+            }
+            return os << " }";
+        }
+
+        template <typename T1, typename T2>
+        std::ostream &operator<<(
+            std::ostream &os,
+            const pair<T1, T2> p)
+        {
+            return cout << '<' << p.first << "," << p.second << ">";
+        }
+
+        #define debug_header()  cout << " (debug) " << setw(12) << __FUNCTION__ << ", " << setw(3) << __LINE__ << "    |    "
+        #define variable(x)     #x << " = " << x
+        #define debug1(x)       debug_header() << variable(x) << endl;
+        #define debug2(x, y)    debug_header() << variable(x) << ", " << variable(y) << endl;
+        #define debug3(x, y, z) debug_header() << variable(x) << ", " << variable(y) << ", " << variable(z) << endl;
+#endif 
+
 //compile: g++ -D DEBUG [arquivo.cpp] -o [arquivo] && ./[arquivo] < input.txt > output.txt
 
 int main () {
-  ios_base::sync_with_stdio(false);
-  return 0;
+    ios_base::sync_with_stdio(false);
+
+    return 0;
 }
